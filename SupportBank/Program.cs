9e5b525159eed84c594f708a9697b95b809eef75
Andrew Huang <Andrew.Huang@softwire.com>
2018-11-07 13:47:22 +0000

    Created classes for accounts and transactions

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -12,47 +12,123 @@ namespace SupportBank
         {
             var pathToFile = @"C:\Users\AYH\Documents\Transactions2014.csv";
             var fileLines = System.IO.File.ReadAllLines(pathToFile);
-            var transactions = new List<Transaction>();
+            var transactions = new TransactionLog();
             foreach (string line in fileLines.Skip(1))
             {
-                Console.WriteLine(line);
-
+                var entries = line.Split(',');
+                transactions.AddTransaction(new Transaction(DateTime.Parse(entries[0]), entries[1], entries[2], entries[3], double.Parse(entries[4])));
             }
-
+            transactions.ListAllTransactions();
+            transactions.ListAccount("Gergana I");
             Console.WriteLine("Press any key to exit.");
             System.Console.ReadKey();
+        }
+
+    }
+
+    internal class Account
+    {
+        public string Name;
+        public double Credit;
+
+        public Account(string name, double credit)
+        {
+            Name = name;
+            Credit = credit;
+        }
+
+        public override string ToString() => $"{Name}, {Credit}";
+    }
+
+    class TransactionLog
+    {
+        private List<Transaction> _transactionList;
+
+        public TransactionLog()
+        {
+            _transactionList = new List<Transaction>();
+        }
+
+        public TransactionLog(List<Transaction> transactionList) => _transactionList = transactionList;
+
+        public void AddTransaction(Transaction transaction) => _transactionList.Add(transaction);
+
+        public void ListAllTransactions()
+        {
+            foreach (Transaction transaction in _transactionList)
+            {
+                Console.WriteLine(transaction);
+            }
+        }
+
+        public List<Account> CreateAccounts()
+        {
+            var accounts = new List<Account>();
+            foreach (Transaction transaction in _transactionList)
+            {
+                // Calculate credit deducted from senders
+                var accountFound = false;
+                foreach (Account account in accounts)
+                {
+                    if (account.Name == transaction.From)
+                    {
+                        account.Credit -= transaction.Amount;
+                        accountFound = true;
+                        break;
+                    }
+                }
+
+                if (!accountFound)
+                {
+                    accounts.Add(new Account(transaction.From, -transaction.Amount));
+                }
+
+                // Calculate credit added to receivers
+                accountFound = false;
+                foreach (Account account in accounts)
+                {
+                    if (account.Name == transaction.To)
+                    {
+                        account.Credit += transaction.Amount;
+                        accountFound = true;
+                        break;
+                    }
+                }
+
+                if (!accountFound)
+                {
+                    accounts.Add(new Account(transaction.To, transaction.Amount));
+                }
+            }
+            return accounts;
+        }
+
+        public void ListAccount(string name)
+        {
+            foreach (Transaction transaction in _transactionList)
+                if (transaction.From == name || transaction.To == name)
+                    Console.WriteLine(transaction);
+            
         }
     }
 
     class Transaction
     {
-        private DateTime date;
-        private string from;
-        private string to;
-        private string narrative;
-        private int amount;
+        public DateTime Date;
+        public string From;
+        public string To;
+        public string Narrative;
+        public double Amount;
 
-        public Transaction()
+        public Transaction(DateTime date, string from, string to, string narrative, double amount)
         {
-            date = new DateTime();
-            from = "";
-            to = "";
-            narrative = "";
-            amount = 0;
+            Date = date;
+            From = from;
+            To = to;
+            Narrative = narrative;
+            Amount = amount;
         }
 
-        public Transaction(string date, string from, string to, string narrative, int amount)
-        {
-            this.date = new DateTime();
-            this.from = from;
-            this.to = to;
-            this.narrative = narrative;
-            this.amount = amount;
-        }
-
-        public void toString()
-        {
-
-        }
+        public override string ToString() => $"{Date.ToShortDateString()}, {From}, {To}, {Narrative}, {Amount}";
     }
 }
